29b7076566d044bf9d8de4568f7c1652192dab62
tribaaldos <tribaaldos@gmail.com>
2023-07-29 11:44:55 -0700

    ho

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import ThreeDWorld from './components/ThreeDWorld';
 
-
 function App() {
     return (
       <div>
         <h1>My 3D World</h1>
         <ThreeDWorld />
+        
       </div>
     );
   };
--- src/components/ThreeDWorld.js
+++ src/components/ThreeDWorld.js
@@ -1,55 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import * as THREE from 'three';
+import { useEffect } from 'react';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const ThreeDWorld = () => {
-  const sceneRef = useRef();
-  const cameraRef = useRef();
-  const rendererRef = useRef();
-
-  useEffect(() => {
-    // Set up the scene
-    const scene = new THREE.Scene();
-    sceneRef.current = scene;
-
-    // Set up the camera
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-    cameraRef.current = camera;
-
-    // Set up the renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    rendererRef.current = renderer;
-    document.body.appendChild(renderer.domElement);
-
-    // Add a cube to the scene
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    // Animation loop
-    const animate = () => {
-      requestAnimationFrame(animate);
-
-      // Rotate the cube
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      // Render the scene with the camera
-      renderer.render(scene, camera);
-    };
-
-    animate();
-
-    // Clean up function
-    return () => {
-      // Remove the renderer's DOM element when the component is unmounted
-      document.body.removeChild(renderer.domElement);
-    };
-  }, []);
-
-  return null; // No need to w render any JSX, everything is done in the useEffect
-};
-
-export default ThreeDWorld;
+export default function ThreeDWorld() {
+  const loader = new GLTFLoader();
+  return (
+    loader.load( '../models/gltf/wolf.gltf')
+  )//
+}
